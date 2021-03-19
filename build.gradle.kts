1f87f7bc4e84e078bc31549f84fa3457155c6f50
diliuskh <diliuskh@gmail.com>
2021-03-19 16:51:56 +0300

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
 
 plugins {
     java
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.4.31"
 }
 
 group = "net.dilius"
 version = "1.0-SNAPSHOT"
+
+val coroutinesVersion = "1.4.3"
+val junitVersion = "5.7.1"
+
 
 repositories {
     mavenCentral()
@@ -14,10 +18,11 @@ repositories {
 dependencies {
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.9")
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-jdk8", "1.3.9")
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-reactor", "1.3.9")
-    testImplementation("junit", "junit", "4.12")
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", coroutinesVersion)
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-jdk8", coroutinesVersion)
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-reactor", coroutinesVersion)
+    testImplementation(platform("org.junit:junit-bom:$junitVersion"))
+    testImplementation("org.junit.jupiter","junit-jupiter")
 }
 
 configure<JavaPluginConvention> {
