201b44cd5760b8ae05f413a552cb5532815af877
diliuskh <diliuskh@gmail.com>
2021-06-01 16:02:08 +0300

    updated dependencies for kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,44 +1,54 @@
-
 plugins {
+    val kotlinVersion = "1.5.10"
     java
-    kotlin("jvm") version "1.4.31"
+    kotlin("jvm") version kotlinVersion
 }
 
 group = "net.dilius"
 version = "1.0-SNAPSHOT"
 
-val coroutinesVersion = "1.4.3"
-val junitVersion = "5.7.1"
-
-
 repositories {
     mavenCentral()
 }
 
+val kotlinVersion = "1.5.10"
+val coroutinesVersion = "1.5.0"
+val junitVersion = "5.8.0-M1"
+val mockitoVersion = "3.10.0"
+val mockitoKotlinVersion = "3.2.0"
+
 dependencies {
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib-jdk8"))
+
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", coroutinesVersion)
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm", coroutinesVersion)
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-jdk8", coroutinesVersion)
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-reactor", coroutinesVersion)
+
     testImplementation(platform("org.junit:junit-bom:$junitVersion"))
-    testImplementation("org.junit.jupiter","junit-jupiter")
+    testImplementation("org.junit.jupiter", "junit-jupiter")
+    testImplementation("org.junit.jupiter", "junit-jupiter-params")
+    testImplementation("org.jetbrains.kotlin", "kotlin-test-junit5", kotlinVersion)
+    testImplementation("org.mockito", "mockito-core", mockitoVersion)
+    testImplementation("org.mockito", "mockito-junit-jupiter", mockitoVersion)
+    testImplementation("org.mockito.kotlin", "mockito-kotlin", mockitoKotlinVersion)
 }
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 tasks {
     compileKotlin {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "1.8"
+            jvmTarget = "11"
         }
     }
     compileTestKotlin {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "1.8"
+            jvmTarget = "11"
         }
     }
 }
